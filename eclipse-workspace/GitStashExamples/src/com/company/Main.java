8c473c37f06766add3ed53dd0cfc4ad826e0e10a
rjayd <rjaybdorado@gmail.com>
2021-11-03 20:16:22 -0400

    the blue ferrari

---
+++ eclipse-workspace/GitStashExamples/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        car newCar = new car();
+
+        newCar.name = "Lambo";
+        newCar.color="";
+        newCar.doors="";
+        newCar.model="aventador";
+        System.out.println(newCar.model);
+    }
+}
