50d2c43cc73181b52f65d92a1d406502629ba6e3
rjayd <rjaybdorado@gmail.com>
2021-11-06 11:53:58 -0400

    featureSun added with a bug that needs to be resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,15 @@ public class Main {
 
         SolarSystem andromeda = new SolarSystem();
         andromeda.sun =1;
-        andromeda.moon=4;
-        andromeda.planet=5;
+        andromeda.moon=2;
+        andromeda.planet=4;
+
+        featureSun acesun = new featureSun();
+        acesun.color="blue";
+        acesun.heat ="60000452145kj";
+        acesun.temp="5 celsius";
+        acesun.radius="0.009";
+        acesun.maritalStatus="single";
+        
     }
 }
